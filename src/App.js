c429f9ccec0b58ab9048f91db13bc6edb5ec0992
MMSDOM\booe <bora.oenal@t-systems.com>
2017-08-08 15:57:55 +0200

    node modules

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React!!!</h2>
+          <h2>Bo starts with React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -19,4 +19,3 @@ class App extends Component {
 }
 
 export default App;
-test
