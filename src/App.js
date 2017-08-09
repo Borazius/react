af1eed5d8ee5309dd96b189e293bbfbc78b9ab2b
MMSDOM\booe <bora.oenal@t-systems.com>
2017-08-09 18:32:03 +0200

    React Kurs Büro - objektorientierung

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// Wir importieren unsere selbst erstellte Komponente
+import Button from './Button';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Bo starts with React</h2>
+          {/* Klasse aufrufen */}
+          <Button></Button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {/* wir verwenden wieder diese Komponente */}
+        <Button></Button>
       </div>
     );
   }
