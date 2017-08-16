9585838e7fdf6330153a468d94742e86b765f8f3
MMSDOM\booe <bora.oenal@t-systems.com>
2017-08-16 17:06:49 +0200

    static.js

---
+++ src/Static.js
@@ -0,0 +1,21 @@
+// Übung "State in React" (Abschnitt 3, Kapitel 14)
+
+// Zur besseren Übersicht wurden die Übungen nicht wie im Videokurs
+// immer nur an der App.js wahrgenommen, weil der alte Code bei jeder neuen 
+// Übung gelöscht wurde um den neuen Code mit den neuen Übungen zu integrieren
+// Wir wollen aber jede Übung nachschlagen können
+
+import React, {Component} from 'react';
+// Wir importieren unsere selbst erstellte Komponente
+
+class Static extends Component {
+  render() {
+    return (
+      <div className="Static">
+        
+      </div>
+    );
+  }
+}
+
+export default Static;
